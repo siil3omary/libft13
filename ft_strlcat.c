192a00a08a46a23900d431f50017b92c9b91261d
siil3omary <l3omary@icloud.com>
2023-11-05 16:33:49 +0100

    done

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,5 +14,24 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+size_t 
+}
+#include <stdio.h>
+#include <string.h>
+#include <bsd/string.h>  // Include the BSD string.h header
 
+int main() {
+    char dest[20] = "Hello, ";
+    const char *source = "world!";
+    
+    // Test strlcat function
+    size_t original_len = strlen(dest);
+    size_t result = strlcat(dest, source,20 );
+    
+    printf("Resulting string: %s\n", dest);
+    printf("Original length: %zu\n", original_len);
+    printf("Final length: %zu\n", strlen(dest));
+    printf("strlcat return value: %zu\n", result);
+    
+    return 0;
 }

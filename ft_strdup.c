8c790d39fb12061fa68ca4f68ad948b154072a37
siil3omary <l3omary@icloud.com>
2023-11-07 14:14:52 +0100

    done

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,10 +6,18 @@
 /*   By: aelomari <aelomari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 02:06:46 by aelomari          #+#    #+#             */
-/*   Updated: 2023/11/07 02:07:39 by aelomari         ###   ########.fr       */
+/*   Updated: 2023/11/07 02:18:28 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s);
+char	*ft_strdup(const char *s)
+{
+	char *ptr;
+	ptr = (char *) malloc(ft_strlen(s) + 1);
+	if(ptr == NULL)
+	return (NULL);
+	ft_strlcpy(ptr  , s ,ft_strlen(s)+ 1);
+	return (ptr);
+}

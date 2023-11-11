6ec796b83b0df9aab024a3a93cc9dc74acddbf6e
siil3omary <l3omary@icloud.com>
2023-11-11 14:36:33 +0100

    done

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aelomari <aelomari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 16:37:06 by aelomari          #+#    #+#             */
-/*   Updated: 2023/11/06 17:59:38 by aelomari         ###   ########.fr       */
+/*   Updated: 2023/11/11 14:35:48 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,6 @@ char	*ft_strchr(const char *s, int c)
 	}
 	return (NULL);
 }
+
+ekrjhtlk3jhrkjhergt
+cd 

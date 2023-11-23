a5c2e975ee023dca5afc416eb3c480c0c24c461e
SII.L3OMARY <L3OMARY@ICLOUD.COM>
2023-11-23 09:06:25 +0100

    done

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -11,58 +11,46 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 		lst = lst->next;
 	}
 }
-void zaza(void *data){
-	char *str = (char *)data;
-	int i = 0;
-	while (str[i])
-	{
-		str[i] = ft_toupper(str[i]);
-		i++;
-	}
+// void zaza(void *data){
+// 	char *str = (char *)data;
+// 	int i = 0;
+// 	while (str[i])
+// 	{
+// 		str[i] = ft_toupper(str[i]);
+// 		i++;
+// 	}
 	
 
-}
-int main()
-{
+// }
+// int main()
+// {
 	
 
-t_list *head = NULL;
+// t_list *head = NULL;
 
 
-t_list *node1 = (t_list *)malloc(sizeof(t_list));
-t_list *node2 = (t_list *)malloc(sizeof(t_list));
-t_list *node3 = (t_list *)malloc(sizeof(t_list));
-t_list *node4 = (t_list *)malloc(sizeof(t_list));
+// t_list *node1 = (t_list *)malloc(sizeof(t_list));
+// t_list *node2 = (t_list *)malloc(sizeof(t_list));
+// t_list *node3 = (t_list *)malloc(sizeof(t_list));
+// t_list *node4 = (t_list *)malloc(sizeof(t_list));
 
-head = node1;
+// head = node1;
 
-node1->content = ft_strdup("slm");
-node1->next = node2;
+// node1->content = ft_strdup("slm");
+// node1->next = node2;
 
-node2->content = ft_strdup("cv");
-node2->next = node3;
+// node2->content = ft_strdup("cv");
+// node2->next = node3;
 
-node3->content = ft_strdup("hi");
-node3->next =node4;
+// node3->content = ft_strdup("hi");
+// node3->next =node4;
 
-node4->content= ft_strdup("Hru");
-node4->next = NULL;
-
-
-
-ft_lstiter(head, &zaza);
+// node4->content= ft_strdup("Hru");
+// node4->next = NULL;
 
 
 
-
-
-
-
-    t_list *current = head;
-    while (current != NULL) {
-        printf("%s\n", (char *)(current->content));
-        current = current->next;
-    }
+// ft_lstiter(head, &zaza);
 
 
 
@@ -70,7 +58,19 @@ ft_lstiter(head, &zaza);
 
 
 
+//     t_list *current = head;
+//     while (current != NULL) {
+//         printf("%s\n", (char *)(current->content));
+//         current = current->next;
+//     }
 
 
 
-}
+
+
+
+
+
+
+
+// }
---
+++ ft_lstmap.c
@@ -0,0 +1,28 @@
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*new_list;
+
+	if (lst && f && del)
+	{
+		new_list = NULL;
+		while (lst)
+		{
+			new = ft_lstnew((*f)(lst->content));
+			if (!new)
+			{
+				ft_lstclear(&new_list, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&new_list, new);
+			new = new->next;
+			lst = lst->next;
+		}
+	}
+	else
+		return (NULL);
+	return (new_list);
+}

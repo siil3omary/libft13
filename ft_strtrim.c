5e5037fc8e8d73778a5c38a2c381dd00b1f9ba4b
Mona Lisa <l3omary@icloud.com>
2023-11-24 10:04:48 +0100

    done

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,29 +6,29 @@
 /*   By: aelomari <aelomari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 04:25:44 by aelomari          #+#    #+#             */
-/*   Updated: 2023/11/12 20:21:22 by aelomari         ###   ########.fr       */
+/*   Updated: 2023/11/24 10:03:57 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "stdio.h"
 
-char	*iffu(size_t start, size_t end)
-{
-	char	*nostr;
+// char	*iffu(size_t start, size_t end)
+// {
+// 	char	*nostr;
 
-	if (start >= end)
-	{
-		nostr = malloc(1);
-		if (nostr == NULL)
-		{
-			return (NULL);
-		}
-		nostr[0] = '\0';
-		return (nostr);
-	}
-	return (NULL);
-}
+// 	if (start >= end)
+// 	{
+// 		nostr = malloc(1 * sizeof(char));
+// 		if (nostr == NULL)
+// 		{
+// 			return (NULL);
+// 		}
+// 		nostr[0] = '\0';
+// 		return (nostr);
+// 	}
+// 	return (NULL);
+// }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -39,11 +39,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	start = 0;
 	end = ft_strlen(s1);
-	nostr = iffu(start, end);
-	if (nostr != NULL)
-	{
-		return (nostr);
-	}
+	// nostr = iffu(start, end);
+	// if (nostr != NULL)
+	// {
+	// 	return (nostr);
+	// }
 	while (ft_strchr(set, s1[start]) && start <= end)
 		start++;
 	while (ft_strchr(set, s1[end - 1]) && end > start)

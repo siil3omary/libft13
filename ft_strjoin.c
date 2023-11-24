0960d7951737c8a3151e7a76b9ffba0b20b3ef43
Mona Lisa <l3omary@icloud.com>
2023-11-24 13:41:55 +0100

    THE LAST DONE

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,36 +6,19 @@
 /*   By: aelomari <aelomari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 20:17:00 by aelomari          #+#    #+#             */
-/*   Updated: 2023/11/24 12:06:15 by aelomari         ###   ########.fr       */
+/*   Updated: 2023/11/24 13:40:53 by aelomari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ifnull(char const *s1, char const *s2)
+static char	*constr(char const *s1, char const *s2)
 {
-
-		if (s1)
-			return (ft_strdup(s1));
-		else if (s2)
-			return (ft_strdup(s2));
-
-
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	i;
-	size_t	ri;
 	size_t	all_len;
 	char	*result;
+	size_t	i;
+	size_t	ri;
 
-	if (!s1 && !s2)
-		return (NULL);
-			if (!s1 || !s2)
-	result = ifnull(s1, s2);
-	if (result)
-		return (result);
 	all_len = ft_strlen(s1) + ft_strlen(s2);
 	result = (char *)malloc((all_len + 1) * sizeof(char));
 	if (!result)
@@ -49,4 +32,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		result[ri++] = s2[i++];
 	result[ri] = '\0';
 	return (result);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	if (!s1 || !s2)
+	{
+		if (s1)
+			return (ft_strdup(s1));
+		else if (s2)
+			return (ft_strdup(s2));
+		else
+			return (NULL);
+	}
+	return (constr(s1, s2));
 }
